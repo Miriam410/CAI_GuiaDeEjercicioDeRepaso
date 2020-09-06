01d3d1b35ab91ff9eba950474796df7f9d1dc94a
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2020-09-06 16:06:52 -0300

    Ejercicio 1

---
+++ Ejercicios/Ejercicios/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int reg = 147;
+
+            for (i = 1; i <= reg; i++)
+            {
+                if(i%3 == 0 && i%5 ==0)
+                {
+                    Console.WriteLine(i + "- FooBar");
+                }
+                else if(i%3 ==0)
+                {
+                    Console.WriteLine(i + "-Foo");
+                }
+                else if (i%5 ==0)
+                {
+                    Console.WriteLine(i + "-Bar");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
