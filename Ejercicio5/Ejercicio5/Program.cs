1ce01a2dc254c6f641c325949c9bb2c745898908
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2020-09-08 20:40:25 -0300

    ejercicio 5

---
+++ Ejercicio5/Ejercicio5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int invertir, a, b, c,d, w, x, y,z;
+            Console.WriteLine("Ingrese un mumero de 5 cifras");
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            a = Num / 10;
+            b = a / 10;
+            c = b / 10;
+            d = Num / 10;
+
+            w = Num %10;
+            x = a %10;
+            y = b%10;
+            z = c % 10;
+
+            invertir = (w*10000) + (x*1000) + (y*100) + (z*10)+ d;
+
+            Console.WriteLine(" El invertido es: "+ invertir);
+            Console.ReadKey();
+        }
+    }
+}
