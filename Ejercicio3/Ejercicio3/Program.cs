6e166dd5695e4d651ece5de8075c6cc5cbeb7e95
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2020-09-08 17:51:27 -0300

    Ejercicio3

---
+++ Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Palindromo = string.Empty;
+            Console.WriteLine("Ingrese una palabra");
+            string Palabra = Console.ReadLine();
+            int i = Palabra.Length;
+
+            for (int j=i -1; j>=0; j--)
+            {
+                Palindromo += Palabra [j];
+            }
+            if (Palindromo == Palabra)
+            {
+                Console.WriteLine(Palabra  + " es palindromo");
+            }
+            else 
+            {
+                Console.WriteLine(Palabra + " no es palindromo");
+            }
+            Console.WriteLine(Palindromo);
+            Console.ReadKey();
+        }
+    }
+}
